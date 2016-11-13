a98a9cfef71965112e59055009b5cf5016674c8b
chadloro <chad.loro.cl@gmail.com>
2016-11-13 00:30:41 -0800

    Update mainPage.php

--- view/public_html/mainPage.php
+++ view/public_html/mainPage.php
@@ -3,16 +3,15 @@
 Welcome to Textbooks@UBC! </br>
 Please find below all the textbooks we currently have for sale. </br>
 Use a filter or the search textbox to narrow down your search.
+
+<a href="Postings.php">Submit a new posting!</a>
 </p>
 <?php
-
 $conn=oci_connect("ora_w5y9a", "a20030145", "ug");
-
 if (!$conn) {
     $e = oci_error();
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-
   // prepare SQL statement for execution
 $a = oci_parse($conn, 'SELECT * FROM Posting');
 if (!$a){
@@ -25,7 +24,6 @@ if(!$b){
 	$e = oci_error($a);
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-
 // Fetch the results of the query
 print "<table border='1'>\n";
 $ncols = oci_num_fields($a);
@@ -41,9 +39,7 @@ while($row = oci_fetch_array($a, OCI_ASSOC + OCI_RETURN_NULLS)){
 	print "</tr>\n";
 }
 print "</table>\n";
-
 oci_free_statement($stid);
 oci_close($conn);
-
 ?>
 </html>
