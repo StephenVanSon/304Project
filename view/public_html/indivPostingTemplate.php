91ed32ea1660afcde6c894d0088b573bca7c34f7
chadloro <chad.loro.cl@gmail.com>
2016-11-18 23:55:33 -0800

    Update indivPostingTemplate.php

--- view/public_html/indivPostingTemplate.php
+++ view/public_html/indivPostingTemplate.php
@@ -1,7 +1,8 @@
 <html>
+
 <?php
 $retrieved_id = $_GET['id'];
-print "This is the page for the post of postId: " . $retrieved_id;
+#print "This is the page for the post of postId: " . $retrieved_id;
 
 $conn=oci_connect("ora_w5y9a", "a20030145", "ug");
 if (!$conn) {
@@ -9,7 +10,12 @@ if (!$conn) {
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
   // prepare SQL statement for execution
-$a = oci_parse($conn, "SELECT t.title, p.price, p.description, p.image, p.ISBN, p.timePosted, u.uName, u.email FROM Posting p, Users u, Textbooks t WHERE p.studentNum = u.studentNum AND p.ISBN = t.ISBN AND p.postId = '$retrieved_id'");
+$a = oci_parse($conn, "SELECT t.title, p.price, p.description, p.image, p.ISBN, p.timePosted, u.uName, u.studentNum, u.email, a.aname
+					   	FROM Posting p, Users u, Textbooks t, Authors_in_Textbook a 
+					   	WHERE p.studentNum = u.studentNum 
+					   		AND p.ISBN = t.ISBN 
+					   		AND t.ISBN = a.ISBN 
+					   		AND p.postId = '$retrieved_id'");
 if (!$a){
 	$e = oci_error($conn);
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
@@ -28,34 +34,46 @@ $desc = $info['DESCRIPTION'];
 $uname = $info['UNAME'];
 $time = $info['TIMEPOSTED'];
 $email = $info['EMAIL'];
+$aname = $info['ANAME'];
+$snum = $info['STUDENTNUM'];
+#$image = $info['IMAGE'];
 
-print "<h1>" . $title . "</h1>\n";
+print "<h1>" . $title . " by " . $aname . "</h1>\n";
 print "<h1>" . $desc . "</h1>\n";
 print "<h2> Posted by: " . $uname . " on " . $time . "</h2>\n";
 print "<h2> Contact " . $email . " for more information </h2>\n";
 
-#print "<table border='1'>\n";
-#$ncols = oci_num_fields($a) - 1;
-#	for ($i = 1; $i <= $ncols; $i++){
-#		$column_name = oci_field_name($a, $i);
-#		print "<th>". $column_name. "</th>";
-#	}
-#
-#while($row = oci_fetch_array($a, OCI_ASSOC + OCI_RETURN_NULLS)){
-#	print "<tr>\n";
-#	foreach($row as $item){
-#		print "<td>".($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
-#		}
-#		print "</tr>\n";
-#	}
-#print "</table>\n";
+if ($snum == $_COOKIE[studNum]) {
+	#print "<button>Hello</button>";
+	print "<button id='editButton'>
+			Edit Description
+		   </button>
+		   <p id='editForm'></p>";
 
-
-#print "Unserialized id: " . $unserialized_id;
-
-
+}
 
 oci_free_statement($stid);
 oci_close($conn);
 ?>
+
+
+<div id="formToEdit"
+	<form action="edit.php" method="post">
+		<input type="text" name="new_description">
+		<input type="hidden" name='postID' value="'$retrieved_id'">
+		<input type="submit" value="Submit Changes">
+	</form>
+</div>
+
+<script src="jquery-3.0.0.js"></script>
+<script>
+$(function () {
+	$("#formToEdit").hide();
+
+	$("#editButton").click(function() {
+		$("#formToEdit").show();
+	})
+});
+</script>
+
 </html>
