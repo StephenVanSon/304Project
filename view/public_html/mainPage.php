fea08f4f2c79d175acf0a69f1579eae52153e038
Ana Barajas <anabarajas@live.com.mx>
2016-11-21 17:20:28 -0800

    Calculate is now only count(*)

--- view/public_html/mainPage.php
+++ view/public_html/mainPage.php
@@ -37,15 +37,15 @@
 					<li class='active'><a href="mainPage.php">Main Page</a></li>
 					<li><a href='Postings.php'>New Posting</a></li>
 					<?php
-						$uname = $_COOKIE["username"];
-						if(empty($uname)){
-							echo "<li><a href='login.php'>Login</a></li>";
-							echo "<li><a href='register.php'>Register</a></li>";
-						}
-						else
-						{
-							echo "<li><a href='logout.php'>Logout</a></li>";
-						}
+					$uname = $_COOKIE["username"];
+					if(empty($uname)){
+						echo "<li><a href='login.php'>Login</a></li>";
+						echo "<li><a href='register.php'>Register</a></li>";
+					}
+					else
+					{
+						echo "<li><a href='logout.php'>Logout</a></li>";
+					}
 					?>
 					
 				</ul>
@@ -74,93 +74,84 @@
 								<div class="panel-body">
 									<form method="post" action="groupBy.php">
 										<div class="radio">
-											<label><input type="radio" name="radio" value="Course">Course</label>
+											<label><input type="radio" name="radio" value="c.courseCode, c.courseNum">Course</label>
 										</div>
 										<div class="radio">
-											<label><input type="radio" name="radio" value="Dept">Department</label>
+											<label><input type="radio" name="radio" value="c.courseCode">Department</label>
 										</div>
 										<div class="radio">
-											<label><input type="radio" name="radio" value="StudNum">Seller Student Number</label>
+											<label><input type="radio" name="radio" value="p.studentNum">Seller Student Number</label>
 										</div>
-										<button type="submit" name="submit" class="btn btn-primary btn-sm">Submit</button>
-									</form>
-									<div class="checkbox">
-										<form method="POST" action="groupBy.php">
-										</br><h4 class="panel-title">Calculate:</h4></br>
-										<label><input type="checkbox" name="apply[]" value="count*">Number of textbooks being sold</br></label>
-										<label><input type="checkbox" name="apply[]" value="average">Average price of textbooks</br></label>
-										<label><input type="checkbox" name="apply[]" value="max">Most expensive textbook</br></label>
-										<label><input type="checkbox" name="apply[]" value="min">Cheapest textbook</br></label></br></br>
-										<button type="submit" class="btn btn-primary btn-sm">Submit</button>
-									</form>
-								</div>
+									<button type="submit" name="submit" class="btn btn-primary btn-sm">Submit</button>
+								</form>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 
-		<div class="col-sm-8 col-sm-offset-4 col-md-9 col-md-offset-3 main">
-			<h1 class="page-header">Check out the textbooks we currently have for sale!</h1>
-			<h4>Use a filter or the search textbox to narrow down your search.</h4>
+	<div class="col-sm-8 col-sm-offset-4 col-md-9 col-md-offset-3 main">
+		<h1 class="page-header">Check out the textbooks we currently have for sale!</h1>
+		<h4>Use a filter or the search textbox to narrow down your search.</h4>
 
-			<div class="table-responsive" id="render">
-				<?php
-				$conn=oci_connect("ora_w5y9a", "a20030145", "ug");
-				if (!$conn) {
-					$e = oci_error();
-					trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-				}
+		<div class="table-responsive" id="render">
+			<?php
+			$conn=oci_connect("ora_w5y9a", "a20030145", "ug");
+			if (!$conn) {
+				$e = oci_error();
+				trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+			}
   					// prepare SQL statement for execution
-				$all_Entries = 'SELECT t.title, p.ISBN, p.description, p.price, p.timePosted, c.courseCode, c.courseNum, p.postId 
-				FROM Posting p, Textbooks t, Course_Of_Textbook c 
-				WHERE p.ISBN = t.ISBN 
-				AND t.ISBN = c.ISBN
-				ORDER BY p.timePosted DESC';
+			$all_Entries = 'SELECT t.title, p.ISBN, p.description, p.price, p.timePosted, c.courseCode, c.courseNum, p.postId 
+			FROM Posting p, Textbooks t, Course_Of_Textbook c 
+			WHERE p.ISBN = t.ISBN 
+			AND t.ISBN = c.ISBN
+			ORDER BY p.timePosted DESC';
 
-				$a = oci_parse($conn, $all_Entries);
-				if (!$a){
-					$e = oci_error($conn);
-					trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-				}
+			$a = oci_parse($conn, $all_Entries);
+			if (!$a){
+				$e = oci_error($conn);
+				trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+			}
 					// Perform logic of the query
-				$b = oci_execute($a);
-				if(!$b){
-					$e = oci_error($a);
-					trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-				}
+			$b = oci_execute($a);
+			if(!$b){
+				$e = oci_error($a);
+				trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+			}
 					// Fetch the results of the query
 
-				echo "<table class='sortable table table-bordered'>";
-				$ncols = oci_num_fields($a) - 2;
-				for ($i = 1; $i <= $ncols; $i++){
-					$column_name = oci_field_name($a, $i);
-					print "<th>". $column_name. "</th>";
-				}
+			echo "<table class='sortable table table-bordered'>";
+			$ncols = oci_num_fields($a) - 2;
+			for ($i = 1; $i <= $ncols; $i++){
+				$column_name = oci_field_name($a, $i);
+				print "<th>". $column_name. "</th>";
+			}
 
-				while($row = oci_fetch_array($a, OCI_ASSOC + OCI_RETURN_NULLS)){
-					print "<tr>\n";
+			while($row = oci_fetch_array($a, OCI_ASSOC + OCI_RETURN_NULLS)){
+				print "<tr>\n";
 						#foreach($row as $item){
-					print "<td><a href='indivPostingTemplate.php?id=".$row['POSTID']."'>".$row['TITLE']."</a></td>";
-					print "<td>".$row['ISBN']."</td>";
-					print "<td>".$row['DESCRIPTION']."</td>";
-					print "<td>".$row['PRICE']."</td>";
-					print "<td>".$row['TIMEPOSTED']."</td>";
-					print "<td>".$row['COURSECODE'] . " " . $row['COURSENUM'] . "</td>";
+				print "<td><a href='indivPostingTemplate.php?id=".$row['POSTID']."'>".$row['TITLE']."</a></td>";
+				print "<td>".$row['ISBN']."</td>";
+				print "<td>".$row['DESCRIPTION']."</td>";
+				print "<td>".$row['PRICE']."</td>";
+				print "<td>".$row['TIMEPOSTED']."</td>";
+				print "<td>".$row['COURSECODE'] . " " . $row['COURSENUM'] . "</td>";
 						#print "<td>".($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
-					print "</tr>\n";
-				}
-				print "</table>\n";
+				print "</tr>\n";
+			}
+			print "</table>\n";
 
 
 
-				oci_free_statement($a);
-				oci_close($conn);
-				?>
-			</div>
+			oci_free_statement($a);
+			oci_close($conn);
+			?>
 		</div>
 	</div>
+</div>
 </div>
 
 
